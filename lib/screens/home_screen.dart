b88fe299789b2826c5cdb6d19e063e98eccf41ac
Hemant <hpaneru.hp@gmail.com>
2023-02-16 11:04:31 +0545

    app bar refactored

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -55,7 +55,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Home Screen',
+          'Home',
         ),
       ),
       body: Center(
